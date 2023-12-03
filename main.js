139a6a2813cbab09683eb5f0474d30f3de4501b7
DanielBenedikt <dbenedict44@gmail.com>
2023-12-03 19:56:27 +0200

    ex2

--- main.js
+++ main.js
@@ -1,18 +1,45 @@
+
 async function apiRequest() {
     const response = await fetch("https://pokeapi.co/api/v2/pokemon")
     const api = await response.json()
-    console.log(api)
+    // console.log(api)
     displayAll(api)
 }
+
 function displayAll(api) {
     let container = document.getElementById("container")
-    let html = "";
-    for(a of api) {
-         html += `  <ul>
+    let html = "<ul>";
+    for(a of api.results) {
+         html += `  
                     <li>${a.name}</li>
-                    </ul>
+                    
                 `      
     }
+        html += "</ul> <hr>"
     container.innerHTML += html
     
 }
+
+async function searchPokemon(){
+    const searchPokemon = document.getElementById("searchPokemon").value
+    const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${searchPokemon}`)
+    const api = await pokemon.json()
+    console.log(api)
+    showDetailsSearch(api)
+}
+
+function showDetailsSearch(api) {
+    let showDetails = document.getElementById("showDetails")
+    let html = `<ul>
+                <li>${api.forms[0].name}</li>
+                <li>${api.types[0].type.name}</li>
+                <li>${api.abilities[0].ability.name}</li>
+                <li>${api.abilities[1].ability.name}</li>
+                </ul>
+            `
+    showDetails.innerHTML += html   
+}
+
+
+
+// abilities.join("")
